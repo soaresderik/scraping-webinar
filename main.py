7b5b9ec05245d572f86e1bb5c2dd897c55dac7b6
Derik Soares <codigoaberto.info@gmail.com>
2019-05-28 21:44:55 -0300

    finished project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 res = requests.get("https://digitalinnovation.one/blog/")
@@ -17,9 +18,22 @@ for link in links:
 for page in all_pages:
     posts = soup.find_all(class_='post')
     for post in posts:
-        author = post.find(class_="post-author").text[5:]
-        title  = post.find('h2').text
+        info = post.find(class_='post-content')
+        title = info.h2.a.text
+        preview = info.p.text
+        date = info.footer.time['datetime']
+        img = post.find(class_='wp-post-image')['src']
+        author = post.find(class_='post-author').text
 
-        all_posts.append({ 'author': author, 'title': title})
+        all_posts.append({
+                'title': title,
+                'preview': preview,
+                'date': date,
+                'author': author[5:],
+                'img': img
+        })
+
 
 print(all_posts)
+with open('posts.json', 'w') as json_file:
+    json.dump(all_posts, json_file, indent=3, ensure_ascii=False)
